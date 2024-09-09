220cf58cf5df4b390447ab47213934fae985389f
Aryanmalaysia <aryanmalikmalaysia@gmail.com>
2024-09-10 01:16:08 +0530

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '85.195.73.202',
-    user: 'win1209_aryanwinuser',
-    password: '729288@Rishav$',
-    database: 'win1209_aryanwin',
+    user: 'win1209_dalilywinuser',
+    password: '76440@Rishav$',
+    database: 'win1209_dailywin',
 });
 
 export default connection;
